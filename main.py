46054fa19bedb904a8f68ff640d79d46085690e0
Ikwan Thareq Kemal <ikwanthareq@gmail.com>
2021-08-13 15:53:16 +0000

    create input and insert into variable

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("Hello " + input("Input your name: ")) 
-print("Hello " + input("Input your name: ")) 
+# print("Hello " + input("Input your name: ")) 
+
+# name = print(input("what's is your name: "))
+# print(name)
+# print("your name " + name)
